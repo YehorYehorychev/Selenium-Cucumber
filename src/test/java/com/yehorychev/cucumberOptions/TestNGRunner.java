7e94163a75a3ae338f4c7d2846a5bea3c513bdcf
Yehor Yehorychev <egor57899@gmail.com>
2024-07-19 18:17:10 -0700

    MainSteps class -> Test definitions added

--- src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
+++ src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/com/yehorychev/features",
-        glue = "stepDefinitions", monochrome = true)
+        glue = "com.yehorychev.stepDefinitions", monochrome = true)
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/com/yehorychev/stepDefinitions/MainSteps.java
+++ src/test/java/com/yehorychev/stepDefinitions/MainSteps.java
@@ -1,25 +1,28 @@
 package com.yehorychev.stepDefinitions;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+
 public class MainSteps {
     @Given("User is on NetBanking landing page")
     public void user_is_on_net_banking_landing_page() {
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User landed on NetBanking page");
     }
+
     @When("User login into application")
     public void user_login_into_application() {
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User login into application");
     }
+
     @Then("Home Page is displayed")
     public void home_page_is_displayed() {
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Home Page is displayed");
     }
-    @And("Cards are displayed")
+
+    @Then("Cards are displayed")
     public void cards_are_displayed() {
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Cards are displayed");
     }
 }
