6598b752bfd889ca3db269b22cfcd0c195edb8bb
Yehor Yehorychev <egor57899@gmail.com>
2024-07-23 18:10:05 -0700

    Reports functionality has been updated

--- src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
+++ src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
@@ -3,7 +3,7 @@ package com.yehorychev.cucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/com/yehorychev/features",
+@CucumberOptions(features = "src/test/java/com/yehorychev/features", dryRun = true,
         glue = "com.yehorychev.stepDefinitions", monochrome = true,
         tags = "@NetBanking or @Mortgage",
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
