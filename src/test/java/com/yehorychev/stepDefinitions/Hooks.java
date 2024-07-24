27a0543748b5313dae31e69e69bed9e8d374eb97
Yehor Yehorychev <egor57899@gmail.com>
2024-07-23 17:47:50 -0700

    Hooks class has been implemented

--- src/test/java/com/yehorychev/stepDefinitions/Hooks.java
+++ src/test/java/com/yehorychev/stepDefinitions/Hooks.java
@@ -1,11 +1,15 @@
 package com.yehorychev.stepDefinitions;
 
+import io.cucumber.java.Before;
+
 public class Hooks {
 
+    @Before("@NetBanking")
     public void netBankingSetup() {
         System.out.println("Setup the entries in NetBank database");
     }
 
+    @Before("@Mortgage")
     public void mortgageSetup() {
         System.out.println("Setup the entries in Mortgage database");
     }
