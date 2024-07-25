439620aa7ef3bf6b89af80f83b71c4615d2c283f
Yehor Yehorychev <egor57899@gmail.com>
2024-07-24 17:36:27 -0700

    Steps definitions class has been created for the GreenKart website

---
+++ src/test/java/com/yehorychev/stepDefinitions/GreenKartStepDefinitions.java
@@ -0,0 +1,25 @@
+package com.yehorychev.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GreenKartStepDefinitions {
+    @Given("User is on GreenKart Landing page")
+    public void user_is_on_green_kart_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User searched with shortname {string} and extracted actual name of product")
+    public void user_searched_with_shortname_and_extracted_actual_name_of_product(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User searched for the same shortname in offers page to check if product exist")
+    public void user_searched_for_the_same_shortname_in_offers_page_to_check_if_product_exist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
