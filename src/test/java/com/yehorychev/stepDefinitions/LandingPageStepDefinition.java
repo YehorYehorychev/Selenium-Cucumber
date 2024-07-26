3be2b532d05de742c772f1b75775581cbe520950
Yehor Yehorychev <egor57899@gmail.com>
2024-07-25 17:48:31 -0700

    stepDefinitions have been changed

---
+++ src/test/java/com/yehorychev/stepDefinitions/LandingPageStepDefinition.java
@@ -0,0 +1,34 @@
+package com.yehorychev.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LandingPageStepDefinition {
+    public WebDriver driver;
+    public String landingPageProductName;
+    public WebDriverWait wait;
+
+    @Given("User is on GreenKart Landing page")
+    public void user_is_on_green_kart_landing_page() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @When("User searched with shortname {string} and extracted actual name of product")
+    public void user_searched_with_shortname_and_extracted_actual_name_of_product(String shortName) throws InterruptedException {
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[normalize-space()='Tomato - 1 Kg']")));
+        landingPageProductName = driver.findElement(By.xpath("//h4[normalize-space()='Tomato - 1 Kg']")).getText().split("-")[0].trim();
+        System.out.println(landingPageProductName + " is extracted from Home Page");
+    }
+}
