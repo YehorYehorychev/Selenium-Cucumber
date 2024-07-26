5a2c3cd3d64c8f6028a12aa3e22da7d2e5e25279
Yehor Yehorychev <egor57899@gmail.com>
2024-07-26 16:52:41 -0700

    LandingPage class has been created with all web elements

--- src/test/java/com/yehorychev/pageObjects/LandingPage.java
+++ src/test/java/com/yehorychev/pageObjects/LandingPage.java
@@ -1,4 +1,19 @@
 package com.yehorychev.pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LandingPage {
+    public WebDriver driver;
+
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By search = By.xpath("//input[@type='search']");
+    By productName = By.xpath("//h4[normalize-space()='Tomato - 1 Kg']");
+
+    public void searchItem(String shortName) {
+        driver.findElement(search).sendKeys(shortName);
+    }
 }
