575953490fec21ef56b18f163c4167ad1c270c1b
Yehor Yehorychev <egor57899@gmail.com>
2024-07-30 17:49:22 -0700

    CheckoutPage class created

--- src/test/java/com/yehorychev/features/checkout.feature
+++ src/test/java/com/yehorychev/features/checkout.feature
@@ -6,8 +6,8 @@ Feature: Place the order for Products
     When User searched with shortname <Name> and extracted actual name of product
     And Added "3" items of the selected product to cart
     Then User proceeds to checkout and validate the <Name> items in checkout page
+    And Verify that the user has ability to enter promo code and place the order
 
     Examples:
     | Name       |
     | Tom        |
-    | Cucumber   |
--- src/test/java/com/yehorychev/pageObjects/CheckoutPage.java
+++ src/test/java/com/yehorychev/pageObjects/CheckoutPage.java
@@ -1,4 +1,31 @@
 package com.yehorychev.pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CheckoutPage {
+    public WebDriver driver;
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By cartBag = By.cssSelector("img[alt='Cart']");
+    private By checkoutButton = By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]");
+    private By promoButton = By.cssSelector(".promoBtn");
+    private By placeOrderButton = By.xpath("//button[contains(text(),'Place Order')]");
+
+
+    public void checkoutItems() {
+        driver.findElement(cartBag).click();
+        driver.findElement(checkoutButton).click();
+    }
+
+    public void verifyPromoButton() {
+        driver.findElement(promoButton).isDisplayed();
+    }
+
+    public void verifyPlaceOrder() {
+        driver.findElement(placeOrderButton).isDisplayed();
+    }
 }
