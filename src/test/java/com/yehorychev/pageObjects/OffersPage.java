ffc8a0b342e602a4f751f83426d3350f83b86e5d
Yehor Yehorychev <egor57899@gmail.com>
2024-08-02 15:27:59 -0700

    CheckoutPage class has been refactored using PageFactory pattern

--- src/test/java/com/yehorychev/pageObjects/OffersPage.java
+++ src/test/java/com/yehorychev/pageObjects/OffersPage.java
@@ -1,27 +1,39 @@
 package com.yehorychev.pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class OffersPage {
     public WebDriver driver;
+    private WebDriverWait wait;
 
     public OffersPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
-    private By search = By.xpath("//input[@type='search']");
-    private By productName = By.cssSelector("tr td:nth-child(1)");
+    @FindBy(xpath = "//input[@type='search']")
+    private WebElement searchInput;
+
+    @FindBy(css = "tr td:nth-child(1)")
+    private WebElement productName;
 
     public void searchItem(String shortName) {
-        driver.findElement(search).sendKeys(shortName);
+        wait.until(ExpectedConditions.visibilityOf(searchInput)).sendKeys(shortName);
     }
 
     public void getSearchItem() {
-        driver.findElement(search).getText();
+        wait.until(ExpectedConditions.visibilityOf(searchInput)).getText();
     }
 
     public String getProductName() {
-        return driver.findElement(productName).getText();
+        return wait.until(ExpectedConditions.visibilityOf(productName)).getText();
     }
 }
