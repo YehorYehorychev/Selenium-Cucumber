f6a700e316a8de626ba4770db2d357900f057b26
Yehor Yehorychev <egor57899@gmail.com>
2024-08-02 15:29:58 -0700

    LandingPage class has been refactored using PageFactory pattern

--- src/test/java/com/yehorychev/pageObjects/LandingPage.java
+++ src/test/java/com/yehorychev/pageObjects/LandingPage.java
@@ -1,47 +1,65 @@
 package com.yehorychev.pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LandingPage {
     public WebDriver driver;
+    private WebDriverWait wait;
 
     public LandingPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
-    private By search = By.xpath("//input[@type='search']");
-    private By productName = By.xpath("//h4[normalize-space()='Tomato - 1 Kg']");
-    private By topDeals = By.xpath("//a[normalize-space()='Top Deals']");
-    private By addToCart = By.cssSelector(".product-action button");
-    private By increment = By.cssSelector("a.increment");
+    @FindBy(xpath = "//input[@type='search']")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//h4[normalize-space()='Tomato - 1 Kg']")
+    private WebElement productName;
+
+    @FindBy(xpath = "//a[normalize-space()='Top Deals']")
+    private WebElement topDeals;
+
+    @FindBy(css = ".product-action button")
+    private WebElement addToCartButton;
+
+    @FindBy(css = "a.increment")
+    private WebElement incrementButton;
 
     public void searchItem(String shortName) {
-        driver.findElement(search).sendKeys(shortName);
+        wait.until(ExpectedConditions.visibilityOf(searchInput)).sendKeys(shortName);
     }
 
     public void getSearchItem() {
-        driver.findElement(search).getText();
+        wait.until(ExpectedConditions.visibilityOf(searchInput)).getText();
     }
 
     public void incrementQuantity(int quantity) {
-        int i = quantity -1;
+        int i = quantity - 1;
         while (i > 0) {
-            driver.findElement(increment).click();
+            wait.until(ExpectedConditions.elementToBeClickable(incrementButton)).click();
             i--;
         }
     }
 
     public void addToCart() {
-        driver.findElement(addToCart).click();
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButton)).click();
     }
 
     public String getProductName() {
-        return driver.findElement(productName).getText();
+        return wait.until(ExpectedConditions.visibilityOf(productName)).getText();
     }
 
     public void selectTopDealsPage() {
-        driver.findElement(topDeals).click();
+        wait.until(ExpectedConditions.elementToBeClickable(topDeals)).click();
     }
 
     public String getTitleLandingPage() {
