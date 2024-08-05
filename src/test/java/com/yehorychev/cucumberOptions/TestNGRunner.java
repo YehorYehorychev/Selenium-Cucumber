b81875515842847c36f9695cce9a5ba4f795c848
Yehor Yehorychev <egor57899@gmail.com>
2024-08-05 10:51:44 -0700

    TestNGRunner class has been updated

--- src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
+++ src/test/java/com/yehorychev/cucumberOptions/TestNGRunner.java
@@ -4,8 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/com/yehorychev/features", dryRun = false,
-        glue = "com.yehorychev.stepDefinitions", monochrome = true,
+@CucumberOptions(
+        features = "src/test/java/com/yehorychev/features",
+        dryRun = false,
+        glue = "com.yehorychev.stepDefinitions",
+        monochrome = true,
         tags = "@PlaceOrder or @OffersPage",
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
